12b4c8bf7a2637379772883e79d164372949c7dc
qalicha-dev28 <qalichanajma44@gmail.com>
2025-06-26 19:59:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(record) {
+  const winningRecord = record.find(game => game.result === "W");
+  return winningRecord ? winningRecord.year : undefined;
+}
